692133b071d23c4b7e356169e9867fd107e80bfe
Hamza <82061179+hxxmz@users.noreply.github.com>
2024-07-11 14:06:43 +0500

    Create vmCreator.py

---
+++ ViewModelCreator/vmCreator.py
@@ -0,0 +1,45 @@
+from os import system, name
+import pyperclip
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+def factory():
+    print("Paste your input. To end the input, press Enter on an empty line.")
+    lines = []
+    while True:
+        try:
+            line = input()
+            if line.strip() == "":
+                break
+            lines.append("public string "+str(line.strip()).replace('\'',"")+" { get; set; }") # public string Name { get; set; }
+        except EOFError:
+            break
+    result = "\n".join(lines)
+    #pyperclip.copy(str(result)) ; 
+    print(result)  
+    return False
+
+def main():
+    exit = False ; loop = False
+    while not exit:
+        if not loop:
+            loop = factory()
+        response = input("\n1. Press enter to exit\n2. 'c'/'cls' to clear screen\n3. 'r'/'re' to restart\n\n>>> ")
+        match response:
+            case ""  | ".":
+                exit = True
+            case "c" | "cls":
+                clear() ; loop = False
+            case "r" | "re":
+                loop = False
+            case _:
+                loop = True
+
+if __name__ == "__main__":
+    main()
